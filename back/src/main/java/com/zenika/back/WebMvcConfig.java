b9327f85aad585999551d30a746ce209966a580a
btravers <btravers@insa-rennes.fr>
2015-05-15 12:18:38 +0200

    Change url pattern

--- back/src/main/java/com/zenika/back/WebMvcConfig.java
+++ back/src/main/java/com/zenika/back/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.zenika.back;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
