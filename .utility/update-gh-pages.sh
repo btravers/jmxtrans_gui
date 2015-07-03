324ae961ebb5e1a24848216735340d1440b7114a
btravers <btravers@insa-rennes.fr>
2015-07-03 16:40:25 +0200

    Update travis script

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,23 +1,20 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update btravers.github.io\n"
+  echo -e "Starting to update gh-pages\n"
 
-  cp backend/target/*.war $HOME/
+  mkdir $HOME/gh-pages
+  cp backend/target/*.war $HOME/gh-pages
 
-  env
-  echo ${GH_TOKEN}
-
-  cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone  https://${GH_TOKEN}@github.com/btravers/btravers.github.io.git
-   #> /dev/null
 
-  cd btravers.github.io
-  cp $HOME/*.war .
+  cd $HOME/gh-pages
+  git init
+  git remote add origin https://${GH_TOKEN}@github.com/btravers/jmxtrans_gui.git > /dev/null
+  git checkout -B gh-pages
 
-  git add *.war
+  git add .
   git commit -am "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
-  git push -fq origin master > /dev/null
+  git push origin gh-pages -fq > /dev/null
 
   echo -e "Done\n"
 fi
