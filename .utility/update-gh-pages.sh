eb0e93a33aeaf400d649cf074e75b310f39391cd
btravers <btravers@insa-rennes.fr>
2015-07-03 15:41:17 +0200

    Update travis script

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,17 +1,17 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update btravers.github.io\n"
 
-  cp backend/target/*.war $HOME/
+  cp backend/pom.xml $HOME/
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=master https://${GH_TOKEN}@github.com/btravers/btravers.github.io.git master > /dev/null
 
-  cd btravers.github.io
-  cp $HOME/*.war .
+  cd master
+  cp $HOME/pom.xml .
 
-  git add *.war
+  git add pom.xml
   git commit -am "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
   git push -fq origin master > /dev/null
 
