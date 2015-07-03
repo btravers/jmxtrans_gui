cac6a8e6d2f4a1ed39a43a8ee466786d41c390dd
btravers <btravers@insa-rennes.fr>
2015-07-03 17:08:32 +0200

    Update travis script

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -2,12 +2,15 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
   mkdir $HOME/gh-pages
-  cp backend/target/*.war $HOME/gh-pages
+  warname=$(ls backend/target/*.war)
+  cp $warname $HOME/gh-pages
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
   cd $HOME/gh-pages
+  echo "<a href=\"https://github.com/btravers/jmxtrans_gui/blob/gh-pages/$warname?raw=true\">war</a>" > index.html
+
   git init
   git remote add origin https://${GH_TOKEN}@github.com/btravers/jmxtrans_gui.git > /dev/null
   git checkout -B gh-pages
