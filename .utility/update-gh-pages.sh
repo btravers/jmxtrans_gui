4a5ed76c80c81cdcab3fc092ddb7e415981f0ae2
btravers <btravers@insa-rennes.fr>
2015-07-03 14:50:24 +0200

    Update travis script

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,19 +1,19 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+  echo -e "Starting to update btravers.github.io\n"
 
-  cp -R backend/target $HOME/target
+  cp backend/target/*.war $HOME/
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/btravers/btravers.github.io.git  gh-pages > /dev/null
+  git clone --quiet https://${GH_TOKEN}@github.com/btravers/btravers.github.io.git
 
-  cd gh-pages
-  cp -Rf $HOME/target/*.war .
+  cd btravers.github.io
+  cp $HOME/*.war .
 
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git add *.war
+  git commit -am "Travis build $TRAVIS_BUILD_NUMBER pushed to btravers.github.io"
+  git push origin master
 
   echo -e "Done magic with coverage\n"
 fi
